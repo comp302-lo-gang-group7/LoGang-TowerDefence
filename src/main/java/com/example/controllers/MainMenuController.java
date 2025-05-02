f42e94ed4b50f176e482bf920c103c66d320be8d
AADaoud <abdullah.daoud03@gmail.com>
2025-05-03 02:04:57 +0300

    Added custom cursor

--- src/main/java/com/example/controllers/MainMenuController.java
+++ src/main/java/com/example/controllers/MainMenuController.java
@@ -3,6 +3,10 @@ package com.example.controllers;
 import com.example.main.Main;
 import javafx.fxml.FXML;
 
+
+/**
+ * This is a controller for the main menu page. It uses the ViewManager to request changes based on button click actions taken in its respective view (FXML file).
+ */
 public class MainMenuController extends Controller {
 
     @FXML
--- src/main/java/com/example/ui/ViewManager.java
+++ src/main/java/com/example/ui/ViewManager.java
@@ -1,27 +1,59 @@
 package com.example.ui;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.ImageCursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * The view manager is used to request screen changes. Whenever a screen is to be loaded into the scene, the view manager handles that process.
  */
 public class ViewManager {
     private final Stage stage;
+    private static Scene scene;
 
     public ViewManager(Stage stage) {
-        this.stage = stage;
+        this.stage = stage; // Ensure it is initialized outside the scope of failure.
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/fxml/home_page.fxml")); // Initially set to home page.
+            Parent root = fxmlLoader.load();
+            this.scene = new Scene(root); // Set initial scene in view manager.
+
+            InputStream stream = getClass().getResourceAsStream("/com/example/assets/ui/01.png");
+            if (stream != null) {
+                Image image = new Image(stream);
+                scene.setCursor(new ImageCursor(image));
+            } else {
+                System.err.println("Custom cursor image not found!");
+            }
+
+        } catch (IOException e) {
+            System.out.printf("An IOException occurred during switch to FXML home page in ViewManager constructor, error: %s%n", e);
+            e.printStackTrace();
+        } catch (Exception unexpectedError) {
+            System.out.printf("An unexpected error occured in ViewManager constructor, error: %s%n", unexpectedError);
+            unexpectedError.printStackTrace();
+        }
     }
 
     public void switchTo(String fxmlPath) {
         try {
+            // Load new fxml page
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
             Parent root = fxmlLoader.load();
-            stage.setScene(new Scene(root));
+
+            // Get original scene with correct setup
+            Scene scene = ViewManager.getScene();
+            scene.setRoot(root);
+
+            // Load a new fxml onto the scene
+            stage.setScene(ViewManager.getScene());
             stage.show();
         } catch (IOException e) {
             System.out.printf("An IOException occurred during switch to FXML path %s, error: %s%n", fxmlPath, e);
@@ -34,4 +66,7 @@ public class ViewManager {
         stage.close();
     }
 
+    public static Scene getScene() {
+        return scene;
+    }
 }
